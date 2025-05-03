59ad89e3e04e06b27eb3c638b0735f8e4b3bf81a
Alice Ridgway <alice.ridgway@zoodigital.com>
2025-05-03 17:07:11 +0100

    adds custom user model

--- cookiecutter/settings.py
+++ cookiecutter/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users"
 ]
 
 MIDDLEWARE = [
@@ -103,6 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "users.CustomUser"  # new
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
