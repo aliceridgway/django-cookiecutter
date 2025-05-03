bb3280c18ae421dadc3fd19ca8857a1a26776ab9
Alice Ridgway <alice.ridgway@zoodigital.com>
2025-05-03 16:56:15 +0100

    Secures secret key

--- cookiecutter/settings.py
+++ cookiecutter/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1zp(0rqg=+!!*h4oh(31pf)fvrc%d@aw#i2kc4@s&)!-exq!n8"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -72,7 +73,6 @@ WSGI_APPLICATION = "cookiecutter.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-import os
 
 DATABASES = {
     "default": {
